01cfb6fb957c65e7d31f0e4e23b13804533c77a6
Muhammedeness <muhammedenes7725@gmail.com>
2025-04-09 11:23:31 +0300

    UserDto and UserSaveDto added

--- src/main/java/com/eteration_project/eteration_project/dto/UserDto.java
+++ src/main/java/com/eteration_project/eteration_project/dto/UserDto.java
@@ -1,4 +1,19 @@
 package com.eteration_project.eteration_project.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
+
+    private String firstName;
+    private String lastName ;
+    private String email;
+
 }
--- src/main/java/com/eteration_project/eteration_project/dto/UserSaveDto.java
+++ src/main/java/com/eteration_project/eteration_project/dto/UserSaveDto.java
@@ -1,4 +1,23 @@
 package com.eteration_project.eteration_project.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+
 public class UserSaveDto {
+
+    @NotEmpty(message = "İsim alanı boş bırakılamaz")
+    @Size(min = 2  , max = 25 , message = "Fazla veya az karakter kullanıldı")
+    private String firstName;
+
+    @NotEmpty(message = "Soyisim alanı boş bırakılamaz")
+    @Size(min = 2 , max = 25 , message = "Fazla veya az karakter kullanıldı")
+    private String lastName;
+
+    private Date birthDate;
+
+    private String email;
+
 }
