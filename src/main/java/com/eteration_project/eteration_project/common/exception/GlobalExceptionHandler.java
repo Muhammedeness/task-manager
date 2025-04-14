26191d1ad1526a30f9ca643d01d3b001380de22d
Muhammedeness <muhammedenes7725@gmail.com>
2025-04-14 11:50:39 +0300

    RuntimeException eklendi

--- src/main/java/com/eteration_project/eteration_project/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eteration_project/eteration_project/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.eteration_project.eteration_project.common.exception;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
@@ -10,29 +12,41 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.util.Locale;
+
 @ControllerAdvice
+@RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(CustomNotFoundException.class)
     @ResponseBody
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public String handleCustomNotFoundException (CustomNotFoundException e){
-        return e.getMessage();
+    public String handleCustomNotFoundException(CustomNotFoundException e) {
+        return messageSource.getMessage(e.getMessage(), null, Locale.getDefault());
+    }
+
+    @ExceptionHandler(CustomRuntimeException.class)
+    @ResponseBody
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public String handeCustomRuntimeException(CustomRuntimeException e) {
+        return messageSource.getMessage(e.getMessage(), null, Locale.getDefault());
     }
 
 
     @ExceptionHandler(CustomDataExistsException.class)
     @ResponseBody
     @ResponseStatus(value = HttpStatus.CONFLICT)
-    public String handleCustomUserExistsException(CustomDataExistsException e){
-        return e.getMessage();
+    public String handleCustomUserExistsException(CustomDataExistsException e) {
+        return messageSource.getMessage(e.getMessage(), null, Locale.getDefault());
     }
 
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
     @ResponseBody
     @ResponseStatus(value = HttpStatus.CONFLICT)
-    public String handleEmptyResultDataAccessException(EmptyResultDataAccessException e){
+    public String handleEmptyResultDataAccessException(EmptyResultDataAccessException e) {
         e.printStackTrace();
         return e.getMessage();
     }
@@ -40,13 +54,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    public ValidationResponseMessage handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+    public ValidationResponseMessage handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
 
         ValidationResponseMessage customMethodArgumentNotValidExceptionMessage = new ValidationResponseMessage();
 
         for (ObjectError objectError : e.getBindingResult().getAllErrors()) {
 
-            String fieldname  = ((FieldError) objectError).getField();
+            String fieldname = ((FieldError) objectError).getField();
             customMethodArgumentNotValidExceptionMessage.addErrorToTheMap(fieldname, objectError.getDefaultMessage());
         }
         return customMethodArgumentNotValidExceptionMessage;
