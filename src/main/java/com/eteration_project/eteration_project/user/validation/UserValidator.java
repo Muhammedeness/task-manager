7da9ef56e8468df67955576f3519066bf15e4a24
Muhammedeness <muhammedenes7725@gmail.com>
2025-04-14 11:48:31 +0300

    Remove ve assigned valid işlemleri eklendi

--- src/main/java/com/eteration_project/eteration_project/user/validation/UserValidator.java
+++ src/main/java/com/eteration_project/eteration_project/user/validation/UserValidator.java
@@ -2,6 +2,9 @@ package com.eteration_project.eteration_project.user.validation;
 
 import com.eteration_project.eteration_project.common.exception.CustomDataExistsException;
 import com.eteration_project.eteration_project.common.exception.CustomNotFoundException;
+import com.eteration_project.eteration_project.common.exception.CustomRuntimeException;
+import com.eteration_project.eteration_project.project.repository.ProjectRepository;
+import com.eteration_project.eteration_project.user.dto.UserDeleteDto;
 import com.eteration_project.eteration_project.user.model.User;
 import com.eteration_project.eteration_project.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +16,26 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserValidator {
     private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
 
     public void isUserExistsCreateValidation(String email) {
         
         if (userRepository.isUserExistsByEmail(email)) {
             throw new CustomDataExistsException("error.user.create");
+        }
+    }
+
+    public void isUserExistsRemoveValidation(String email) {
+
+        if (!userRepository.isUserExistsByEmail(email)) {
+            throw new CustomNotFoundException("error.user.not.found");
+        }
+    }
+
+    public void isUserAssignedValidation(UserDeleteDto userDeleteDto){
+
+        if (projectRepository.isUserAssigned(userDeleteDto)) {
+            throw new CustomRuntimeException("error.user.removed");
         }
     }
 }
