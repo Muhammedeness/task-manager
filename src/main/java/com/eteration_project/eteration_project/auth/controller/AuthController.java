9f11d8f32eae1e9419c3af49e2931d4496cb5293
Muhammedeness <muhammedenes7725@gmail.com>
2025-04-28 10:44:43 +0300

    token aktifliğini sorgulayan token inspect endpointi için controller yazıldı

--- src/main/java/com/eteration_project/eteration_project/auth/controller/AuthController.java
+++ src/main/java/com/eteration_project/eteration_project/auth/controller/AuthController.java
@@ -2,15 +2,14 @@ package com.eteration_project.eteration_project.auth.controller;
 
 import com.eteration_project.eteration_project.auth.dto.LoginRequestDto;
 import com.eteration_project.eteration_project.auth.dto.LoginResponseDto;
+import com.eteration_project.eteration_project.auth.dto.TokenInspectDto;
 import com.eteration_project.eteration_project.auth.service.AuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -29,4 +28,10 @@ public class AuthController {
             return ResponseEntity.ok(response);
 
     }
+
+    @PostMapping(path = "/inspect")
+    public ResponseEntity<TokenInspectDto> inspect(@RequestParam("token") String token) {
+        return ResponseEntity.ok(loginService.inspectToken(token));
+    }
+
 } 
