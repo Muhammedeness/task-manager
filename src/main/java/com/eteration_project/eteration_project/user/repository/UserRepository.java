48754399035a4ef5fd6877a7b5fd9126dc8a5a86
Muhammedeness <muhammedenes7725@gmail.com>
2025-04-29 15:53:21 +0300

    kod düzenlemeleri

--- src/main/java/com/eteration_project/eteration_project/user/repository/UserRepository.java
+++ src/main/java/com/eteration_project/eteration_project/user/repository/UserRepository.java
@@ -1,21 +1,16 @@
 package com.eteration_project.eteration_project.user.repository;
 
-import com.eteration_project.eteration_project.project.dto.ProjectDetailsDto;
-import com.eteration_project.eteration_project.project.dto.ProjectResponseDto;
 import com.eteration_project.eteration_project.user.dto.UserDeleteDto;
-import com.eteration_project.eteration_project.user.dto.UserResponseDto;
 import com.eteration_project.eteration_project.user.dto.UserSaveDto;
 import com.eteration_project.eteration_project.user.model.User;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface UserRepository {
 
     User save(UserSaveDto userSaveDto);
     User findUserByEmail(String email);
-    //Optional<User> findUserByEmail(String email);
-
 
     List<User> getAllUsers();
 
